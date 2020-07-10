20addc8afc38b4b92656a0140471a2d37315d469
raimon <raimon49@hotmail.com>
2020-07-10 17:03:22 +0900

    Use if-let expression with int value

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,15 @@ fn main() {
         2 => println!("User Asleep"),
         _ => println!("Unrecognized Error {}", code)
     }
+
+    let value = Some(1);
+    // if let式はパターンがひとつしかないmatch式
+    if let Some(v) = value {
+        println!("Some value is {}", v)
+    }
+    // match式でパターンマッチ
+    match value {
+        Some(v) => println!("Some value is {}", v),
+        None => println!("Some value is None")
+    }
 }
